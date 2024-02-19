dc6dc43fafdb7a897383a60b7e0be05eddba19cf
irfan shah <programmer.irfansha@hotmail.com>
2024-02-19 22:53:57 +0500

    add app.css in app.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import '../public/CustomCSS/style.css'
+import './App.css'
 import Home from "./Components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Helmet } from "react-helmet";
