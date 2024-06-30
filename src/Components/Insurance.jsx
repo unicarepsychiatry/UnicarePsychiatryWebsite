4b9e38eb4b1bb11a87ae7da0b2e9086ee79c6a31
Mariola Hullings <mariolahullings@gmail.com>
2024-06-30 18:26:36 -0400

    Updates insurance Info

--- src/Components/Insurance.jsx
+++ src/Components/Insurance.jsx
@@ -5,7 +5,7 @@ const Insurance = () => {
     <div className="insurance-bg p-5">
       <div className="container">
         <div className="">
-          <h2 className="text-uppercase p-4 text-white f-30-b text-center shadow-lg ">insurance</h2>
+          <h2 className="text-uppercase p-4 text-black f-20-b text-center shadow-lg ">Accepted Insurance Providers In Kentucky:</h2>
         </div>
         <div className="container p-0 m-0">
         <div className="row g-3 p-0 my-4 m-auto text-center d-flex justify-content-center">
@@ -22,6 +22,8 @@ const Insurance = () => {
             <div className="col-md-4 col-6"><img className="img-fluid insurance-logos" src="Images/CareSource.webp"/></div>
             <div className="col-md-4 col-6"><img className="img-fluid insurance-logos" src="Images/MolinaHealthcare.png"/></div>
             <div className="col-md-4 col-6"><img className="img-fluid insurance-logos" src="Images/wellcare-health-plans-logo.png"/></div>
+
+            <h2 className="text-uppercase p-4 text-black f-20-b text-center shadow-lg ">Please note that in Iowa, Washington, and Wisconsin, we accept only cash payments.</h2>
         </div>
         </div>
       </div>
