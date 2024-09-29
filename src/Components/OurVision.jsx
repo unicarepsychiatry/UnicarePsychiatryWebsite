7a221774c76432cfed582ef23c20854240516b91
Mariola Hullings <mariolahullings@gmail.com>
2024-09-29 14:30:20 -0400

    updates

--- src/Components/OurVision.jsx
+++ src/Components/OurVision.jsx
@@ -8,14 +8,14 @@ export const OurVision = () => {
           <div className="order-lg-1 order-2 col-lg-6 col-md-6 col-12 d-flex align-items-center justify-content-center text-center">
             <div className="" data-aos="fade-down-left">
               <p className="f-18-b text-white">
-                {/* The vision of Uni-care Psychiatry is rooted in addressing the
+                The vision of Uni-care Psychiatry is rooted in addressing the
                 scarcity of psychiatric care, particularly in regions with
                 limited access, including addiction treatment. Uni-care
                 Psychiatry expands its services to Wisconsin and will soon
                 extend to New York and Iowa through telehealth, tackling the
                 urgent need for mental health services in underserved
                 populations. We collaborate with mental health clinicians who
-                share our vision, amplifying our impact and reach. */}
+                share our vision, amplifying our impact and reach.
               </p>
               {/* <button
                 className="my-3 px-md-5 p-2 btn button-primary text-white"
